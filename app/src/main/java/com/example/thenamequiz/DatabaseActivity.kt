17785dee40ddb5c0aac7f2c4f2e9ee3180973618
Niklas Johansson <576966@stud.hvl.no>
2021-01-28 11:25:37 +0100

    Added activities

---
+++ app/src/main/java/com/example/thenamequiz/DatabaseActivity.kt
@@ -0,0 +1,11 @@
+package com.example.thenamequiz
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DatabaseActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_database)
+    }
+}
