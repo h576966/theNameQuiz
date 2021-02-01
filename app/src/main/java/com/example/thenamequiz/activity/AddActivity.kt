2f1bfdbab8da30c918b25e4e63613236e17952cb
Niklas Johansson <576966@stud.hvl.no>
2021-02-01 17:27:22 +0100

    AddActivity.kt

--- app/src/main/java/com/example/thenamequiz/activity/AddActivity.kt
+++ app/src/main/java/com/example/thenamequiz/activity/AddActivity.kt
@@ -1,34 +1,49 @@
 package com.example.thenamequiz.activity
 
+import android.content.Intent
 import android.graphics.Bitmap
 import android.os.Bundle
-import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 import com.example.thenamequiz.databinding.ActivityAddBinding
 import com.example.thenamequiz.model.Person
 import com.example.thenamequiz.model.Shared
 import com.example.thenamequiz.R
+import android.content.Context;
+import android.net.Uri;
+import android.provider.MediaStore;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.*
+import java.io.IOException
 
 
 class AddActivity : AppCompatActivity() {
-    private val personList // A list to store the photos in
+    private var personList // A list to store the photos in
             : ArrayList<Person>? = null
     private val PICK_IMAGE_REQUEST = 100 // the request code defined as an instance variable
 
-    private val iv: ImageView? = null
-    private val name: String? = null
-    private val image: Bitmap? = null
+    private var iv: ImageView? = null
+    private var name: String? = null
+    private var image: Bitmap? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding = ActivityAddBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        iv = (ImageView) findViewById(R.id.imageView);
+        iv = binding.imageView
 
         personList = Shared.getPersonList(); // Get the full list from data structure
 
-        Button btn = (Button)findViewById(R.id.imageBtn);       //The button is created´"Velg bilde"
+        binding.imageBtn.setOnClickListener {
+            val shareIntent = Intent()
+            shareIntent.action = Intent.ACTION_GET_CONTENT
+            shareIntent.type = "image/*"
+            startActivityForResult(intent, PICK_IMAGE_REQUEST)
+
+
+        }
+        /* Button btn = (Button)findViewById(R.id.imageBtn);       //The button is created´"Velg bilde"
         btn.setOnClickListener(new View.OnClickListener() {            //The action is created
             public void onClick(View v) {                         //Make it possible for the user to add an image
                 Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
@@ -36,8 +51,62 @@ class AddActivity : AppCompatActivity() {
                 startActivityForResult(intent, PICK_IMAGE_REQUEST);
             }
         });
+        */
+        */
+        binding.addBtn.setOnClickListener {
+            fun onClick(view: View) {
 
-        Button btn2 = (Button)findViewById(R.id.addBtn);       //The button is created´"Legg til"
+                var editText = binding.editTextName
+               name = binding.editTextName.text.toString()
+
+                var text = ""
+                var added = false
+
+                if (!name.equals("")&& image != null){
+                    PersonList.addPerson(name, image)
+                    added = true
+                    text = "Picture was added"
+                }else { text = "Add text or image" }
+
+                var context = applicationContext //might be wrong
+                var duration = Toast.LENGTH_SHORT
+                var toast = Toast.makeText(context, text, duration);  //creating the Toast
+
+                toast.show()
+
+                if(added){                                                  //If image is not added
+                    editText.getText().clear();                              //empty the editText
+                    iv!!.setImageResource(0);                                   //remove image
+                }
+            }
+
+        }
+
+         fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+            super.onActivityResult(requestCode, resultCode, data)
+
+            when(requestCode) {
+
+                PICK_IMAGE_REQUEST-> if (resultCode == RESULT_OK){
+                val selectedImage = data.getData()
+                try{
+                    val bitmap = MediaStore.Images.Media.getBitmap(contentResolver, selectedImage)
+
+                    image = bitmap
+                    iv!!.setImageBitmap(bitmap)
+                } catch (e:IOException){
+                    e.printStackTrace()
+                }
+
+                }
+            }}
+        }
+        }
+
+
+
+
+           /* Button btn2 = (Button)findViewById(R.id.addBtn);       //The button is created´"Legg til"
         btn2.setOnClickListener(new View.OnClickListener() {      //The action is created
 
             public void onClick(View v) {
