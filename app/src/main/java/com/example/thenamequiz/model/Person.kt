9bee6c86539daa179003122c103d81e7b1261c74
H580865 <Darren_kul@hotmail.com>
2021-02-01 15:07:27 +0100

    Gjorde om person-klassen

--- app/src/main/java/com/example/thenamequiz/model/Person.kt
+++ app/src/main/java/com/example/thenamequiz/model/Person.kt
@@ -5,6 +5,33 @@ import java.util.*
 
 
 class Person {
+
+    //Ny
+    private var name: String? = null
+    private var image: Bitmap? = null
+
+    fun Person(name: String?, img: Bitmap) {
+        this.name = name
+        this.image = img
+    }
+
+    fun getName(): String? {
+        return name
+    }
+
+    fun getImage(): Bitmap? {
+        return image
+    }
+
+
+
+
+
+
+
+
+
+    //Gammel
     private var imageName: String? = null
     private var ID: String? = null
     private val image: Bitmap? = null
