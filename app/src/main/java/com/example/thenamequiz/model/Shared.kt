6ca7c763055caf1d614e429b4246896ea1312e36
H580865 <Darren_kul@hotmail.com>
2021-02-01 15:21:54 +0100

    Gjorde om shared-klassen

--- app/src/main/java/com/example/thenamequiz/model/Shared.kt
+++ app/src/main/java/com/example/thenamequiz/model/Shared.kt
@@ -1,9 +1,36 @@
 package com.example.thenamequiz.model
 
+import android.R
 import android.app.Application
-import java.util.ArrayList
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import java.util.*
+
 
 class Shared : Application() {
+
+   //ny
+   private var PersonList: ArrayList<Person?>? = null
+
+    fun PersonList() {
+        PersonList = ArrayList<Person>()
+    }
+
+    fun addPerson(name: String?, image: Bitmap?) {
+        val person = Person("hassan", BitmapFactory.decodeResource(this.resources, R.drawable.hassan))
+        PersonList!!.add(person)
+    }
+
+    fun getPersonList(): ArrayList<Person?>? {
+        return PersonList
+    }
+
+    fun setPersonList(PersonList: ArrayList<Person?>?) {
+        this.personList = personList
+    }
+
+
+    // gammel
     private var persons: ArrayList<Person>? = null
 
     fun SharedObject() {
